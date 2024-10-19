c502ebe60816078798bb2e5a3a034e607e5ee80e
irf4n4ik <167031850+irf4n4ik@users.noreply.github.com>
2024-10-19 11:11:07 +0300

    Update 6.c

--- 1/6/6.c
+++ 1/6/6.c
@@ -8,34 +8,6 @@ double func_b(double x);
 double func_c(double x);
 double func_d(double x);
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <epsilon>\n", argv[0]);
-        return 1;
-    }
-
-    double epsilon = atof(argv[1]);
-    if (epsilon > 1) {
-        printf("incorrect epsilon\n");
-        return 1;
-    }
-
-    double result_a = integral(func_a, epsilon, 1, epsilon);
-    double result_b = integral(func_b, 0, 1, epsilon);
-    double result_c = integral(func_c, 0, 1 - epsilon, epsilon);
-    double result_d = integral(func_d, epsilon, 1, epsilon);
-
-    printf("---------------------------------------------------------\n");
-    printf("Integral Function       | Result\n");
-    printf("------------------------|------------------------\n");
-    printf("Integral a (ln(1+x)/x)  | %.10f\n", result_a);
-    printf("Integral b (exp(-x^2/2))| %.10f\n", result_b);
-    printf("Integral c (ln(1/(1-x)))| %.10f\n", result_c);
-    printf("Integral d (x^x)        | %.10f\n", result_d);
-    printf("------------------------|------------------------\n");
-
-    return 0;
-}
 
 double integral(double (*func)(double), double a, double b, double eps) {
     double n = 1.0;
@@ -74,4 +46,33 @@ double func_c(double x) {
 double func_d(double x) {
     if (x == 0) return 0;
     return pow(x, x);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <epsilon>\n", argv[0]);
+        return 1;
+    }
+
+    double epsilon = atof(argv[1]);
+    if (epsilon > 1) {
+        printf("incorrect epsilon\n");
+        return 1;
+    }
+
+    double result_a = integral(func_a, epsilon, 1, epsilon);
+    double result_b = integral(func_b, 0, 1, epsilon);
+    double result_c = integral(func_c, 0, 1 - epsilon, epsilon);
+    double result_d = integral(func_d, epsilon, 1, epsilon);
+
+    printf("---------------------------------------------------------\n");
+    printf("Integral Function       | Result\n");
+    printf("------------------------|------------------------\n");
+    printf("Integral a (ln(1+x)/x)  | %.10f\n", result_a);
+    printf("Integral b (exp(-x^2/2))| %.10f\n", result_b);
+    printf("Integral c (ln(1/(1-x)))| %.10f\n", result_c);
+    printf("Integral d (x^x)        | %.10f\n", result_d);
+    printf("------------------------|------------------------\n");
+
+    return 0;
 }
